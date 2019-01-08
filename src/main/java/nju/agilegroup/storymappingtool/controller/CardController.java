db903b740cfb222884f20f68862aafbb0890c674
785693057 <785693057@qq.com>
2019-01-08 10:53:04 +0800

    在注释补充了接口返回值说明

--- src/main/java/nju/agilegroup/storymappingtool/controller/CardController.java
+++ src/main/java/nju/agilegroup/storymappingtool/controller/CardController.java
@@ -25,6 +25,7 @@ public class CardController {
      * 获得一个地图的所有卡片，卡片以结构化的方式返回
      * 测试URL：http://localhost:8090/getMapCards?mapId=1
      * 按照层次结构返回了卡片列表
+     * 返回类型：List<ActivityCardInfo>的json
      * @param request
      * @param mapId
      * @return
@@ -38,6 +39,8 @@ public class CardController {
     /**
      * 创建一个卡片
      * 输入json：cardInfo，需要填写的字段在CardInfo类中
+     * 返回对应卡片类型的CardInfo的json
+     * PS：输入的CardInfo类只用来上传
      * @param request
      * @param cardInfo
      * @return
@@ -63,6 +66,7 @@ public class CardController {
     /**
      * 修改一张卡片，需要传入卡片ID
      * 这里createAt字段会刷新为修改时间，如果觉得没问题可以在界面上显示为最后修改于……因为不改的话可能需要多查一次表
+     * 返回对应卡片类型的CardInfo的json
      * @param request
      * @param cardInfo
      * @return
@@ -85,12 +89,26 @@ public class CardController {
         return cardService.modifyStoryCard(session, cardInfo);
     }
 
+    /**
+     * 获取一个地图的所有角色
+     * 返回List<RoleInfo>的json
+     * @param request
+     * @param mapId
+     * @return
+     */
     @RequestMapping(value = "/getRoles", method = RequestMethod.GET)
     public ResultInfo<Object> getRoles(HttpServletRequest request, @RequestParam int mapId){
         HttpSession session = request.getSession();
         return cardService.getRoles(session, mapId);
     }
 
+    /**
+     * 创建一个角色
+     * 返回RoleInfo的json
+     * @param request
+     * @param roleInfo
+     * @return
+     */
     @RequestMapping(value = "/createRole", method = RequestMethod.POST)
     public ResultInfo<Object> createRole(HttpServletRequest request, @RequestBody RoleInfo roleInfo){
         HttpSession session = request.getSession();
